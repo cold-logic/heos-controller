b7d58ca33cef6f9481ea44d2cfc0ccdaaa1cf8f1
Christopher Davison <chris@spoofwear.com>
2017-08-17 12:27:30 -0700

    Add auto-update

--- app/index.js
+++ app/index.js
@@ -2,6 +2,12 @@
 
 const {app, BrowserWindow, Menu} = require("electron");
 const menu = require("./menu.js")
+const log = require('electron-log')
+const {autoUpdater} = require("electron-updater")
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -16,10 +22,44 @@ app.on("window-all-closed", function() {
   // }
 });
 
+// Handle Updates
+autoUpdater.on('checking-for-update', () => {
+  log.info('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  log.info('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  log.info('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  log.info('Error in auto-updater.');
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  log.info(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  log.info('Update downloaded; will install in 5 seconds');
+});
+autoUpdater.on('update-downloaded', (info) => {
+  // Wait 5 seconds, then quit and install
+  // In your application, you don't need to wait 5 seconds.
+  // You could call autoUpdater.quitAndInstall(); immediately
+  setTimeout(function() {
+    autoUpdater.quitAndInstall();  
+  }, 5000)
+})
+
 // This method will be called when Electron has done everything
 // initialization and ready for creating browser windows.
 app.on("ready", function() {
   var appPath = require("./app-path.js")();
+
+  // Check for updates
+  autoUpdater.checkForUpdates();
 
   // Create the browser window.
   mainWindow = new BrowserWindow({
