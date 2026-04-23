0194902f179c10a9972ae5b1d4cdc4a7350ea6ec
cold-logic <1334924+cold-logic@users.noreply.github.com>
2026-04-23 11:41:37 -0700

    feat: implement local import maps and refactor renderer to ESM

--- app/browser/index.js
+++ app/browser/index.js
@@ -1,11 +1,12 @@
 /* global $ */
 
-window.$ = window.jQuery = require('jquery');
-require('jquery-knob/dist/jquery.knob.min.js');
+import $ from 'jquery';
+window.$ = window.jQuery = $;
+import 'jquery-knob';
 
-const dgram = require('node:dgram'); // dgram is UDP
-const net = require('node:net');
-const os = require('node:os'); 
+import dgram from 'node:dgram'; // dgram is UDP
+import net from 'node:net';
+import os from 'node:os'; 
 
 const HEOS_BROADCAST_ADDR = "239.255.255.250";
 let connection, ui, lastCommand, discovery_interval;
